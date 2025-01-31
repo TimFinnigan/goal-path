71fbe1640a72bdf55dd2c4122773cad07d40558b
Tim Finnigan <8531914+TimFinnigan@users.noreply.github.com>
2025-01-30 21:40:55 -0800

    enable grab cursor

--- main.js
+++ main.js
@@ -1,17 +1,11 @@
-const { app, BrowserWindow, screen } = require("electron");
+const { app, BrowserWindow, ipcMain } = require("electron");
 
 let win;
 
 app.whenReady().then(() => {
-    const primaryDisplay = screen.getPrimaryDisplay();
-    const { width, height } = primaryDisplay.workAreaSize;
-
-    const windowWidth = 250;
-    const windowHeight = 50;
-
     win = new BrowserWindow({
-        width: windowWidth,
-        height: windowHeight,
+        width: 250,
+        height: 50,
         x: 0,
         y: 0,
         alwaysOnTop: true,
@@ -21,6 +15,7 @@ app.whenReady().then(() => {
         backgroundColor: "#00000000",
         webPreferences: {
             nodeIntegration: true,
+            contextIsolation: false,  // Allow ipcRenderer to communicate
         },
     });
 
@@ -28,26 +23,28 @@ app.whenReady().then(() => {
     win.setAlwaysOnTop(true, "screen-saver");
     win.loadFile("index.html");
 
-    // Allow normal mouse interactions with elements (Fix for transparent windows)
-    win.setIgnoreMouseEvents(false, { forward: true });
+    // Ensure interactions are allowed
+    win.setIgnoreMouseEvents(false);
+});
 
-    if (!app.isPackaged) {
-        win.webContents.on("devtools-opened", () => {
-            win.webContents.closeDevTools();
+ipcMain.on("move-window", (event, { deltaX, deltaY }) => {
+    if (win) {
+        const bounds = win.getBounds();
+        win.setBounds({
+            x: bounds.x + deltaX,
+            y: bounds.y + deltaY,
+            width: bounds.width,
+            height: bounds.height
         });
-    } else {
-        win.webContents.closeDevTools();
     }
 });
 
-// Close app when all windows are closed (except macOS)
 app.on("window-all-closed", () => {
     if (process.platform !== "darwin") {
         app.quit();
     }
 });
 
-// Recreate window on macOS when clicked
 app.on("activate", () => {
     if (BrowserWindow.getAllWindows().length === 0) {
         app.whenReady().then(() => {
@@ -63,14 +60,13 @@ app.on("activate", () => {
                 backgroundColor: "#00000000",
                 webPreferences: {
                     nodeIntegration: true,
+                    contextIsolation: false,
                 },
             });
-
             win.setVisibleOnAllWorkspaces(true, { visibleOnFullScreen: true });
             win.setAlwaysOnTop(true, "screen-saver");
             win.loadFile("index.html");
-
-            win.setIgnoreMouseEvents(false, { forward: true });
+            win.setIgnoreMouseEvents(false);
         });
     }
 });
